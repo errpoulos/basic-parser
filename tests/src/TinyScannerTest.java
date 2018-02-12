de43505c5d926fe57459f3c552a2eead46e9528d
errpoulos <erpoulos@unca.edu>
2018-02-12 11:54:13 -0500

    Added test

---
+++ tests/src/TinyScannerTest.java
@@ -0,0 +1,16 @@
+package src;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TinyScannerTest {
+
+
+
+    @Test
+    void nextToken() {
+    }
+}
---
+++ tests/src/TokenizerTest.java
@@ -0,0 +1,23 @@
+package src;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokenizerTest {
+
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void tokenize() {
+    }
+
+    @Test
+    void getTokens() {
+    }
+}
